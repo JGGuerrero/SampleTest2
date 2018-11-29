1d1e764b58a51e6767a2ee2b78b56c2c5a05b972
JGGuerrero <jgguerrero@mail.fresnostate.edu>
2018-11-28 21:57:18 -0800

    finish exam

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,16 +3,50 @@ import { LocalStorageService } from '../localStorageService';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: IUser = {
+    username: null,
+    password: null
+  };
+
   constructor(private router: Router, private toastService: ToastService) {
   }
 
   ngOnInit() {
+
+  }
+
+  login(user: IUser) {
+    console.log('from login user: ', user);
+
+    const presetUser = { username: 'Joshuah', password: 'Josh123' };
+
+    if (user.username != null && user.password != null && user.username != '' && user.password != '') {
+      console.log('from within if statement...')
+      if (user.username == presetUser.username && user.password == presetUser.password) {
+        localStorage.setItem('contacts', JSON.stringify(user));
+        this.router.navigate(['contacts', user]);
+
+      } else {
+        this.toastService.showToast('warning', 2000, 'Username or password is incorrect');
+
+      }
+    } else {
+      console.log('Must specify credentials');
+      this.toastService.showToast('danger', 2000, 'must specify credentials');
+    }
 
   }
 
--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from '../toast/toast.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastService: ToastService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  showAbout() {
+    this.toastService.showToast('success', 5000, 'this application was created by Joshuah Guerrero (c).');
+  }
+
+
+  logout() {
+    localStorage.setItem('user', JSON.stringify({}))
+    this.router.navigate(['login']);
   }
 
 }
